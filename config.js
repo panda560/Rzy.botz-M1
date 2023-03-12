6f72d2194ffcb41ca73f16ce74c0c804679b573a
Muhammad Arrezza Faiz Pratama <103143006+panda560@users.noreply.github.com>
2023-03-12 12:37:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 
 let fs = require('fs')
-global.owner = ['6285785694474', '6285785694474','6285828764046','6285828764046','6285828764046'] // Letakan nomor kamu disini
+global.owner = ['6281273677810', '6281959293465','6285609467937','855389162590','82181337337'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
@@ -40,8 +40,8 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'Rzy-bot'
-global.author = 'Rzy-bot'
+global.packname = 'PANDA BOTZ'
+global.author = 'FAIZTZY'
 
 global.wait = '「 ⏱️ 」Harap tunggu sebentar...'
 global.eror = '「❗」Server ERROR!'
